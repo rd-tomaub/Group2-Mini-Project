907bfcae0574ecd9a730054e57092e23a758230f
rd-tomaub <145316139+rd-tomaub@users.noreply.github.com>
2023-10-02 13:27:35 +0800

    Added initial classes

---
+++ SeatLayout.java
@@ -0,0 +1,12 @@
+
+public class SeatLayout {
+	// just follow class diagram and provide setters/getters
+	
+	
+	// public boolean unreserveSeat(String position){}
+/*
+	 sample input parameter: A1 
+	 since seatLayout are stored in a hashmap, 
+	 the key (or position) would be a String and its value is also a String,
+*/
+}
