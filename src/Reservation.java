33fd7584ecb9e7e83f5120ef91b041be45f10b1f
Ayannrwz <enriquezadrian.aws@gmail.com>
2023-10-02 14:52:27 +0800

    Modified Reservation model java file

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,4 +1,26 @@
 
 public class Reservation {
+	private short reservationNum;
+	private String seatCodes;
+	private MovieSchedule movie;
+	
+	public Reservation(short reservationNum, String seatCodes, MovieSchedule movie) {
+		super();
+		this.reservationNum = reservationNum;
+		this.seatCodes = seatCodes;
+		this.movie = movie;
+	}
 
+	public short getReservationNum() {
+		return reservationNum;
+	}
+
+	public String getSeatCodes() {
+		return seatCodes;
+	}
+
+	public MovieSchedule getMovie() {
+		return movie;
+	}
+	
 }
