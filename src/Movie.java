3c1ec7e20771963767e092c7d3786a31ff4085a6
Ayannrwz <enriquezadrian.aws@gmail.com>
2023-10-02 14:42:27 +0800

    Modified the Movie and MovieSchedule class

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,32 @@
 
 public class Movie {
+	private short movieId;
+	private String moviewName;
+	private float duration;
+	private byte cinemaNum;
+	
+	public Movie(short movieId, String moviewName, float duration, byte cinemaNum) {
+		super();
+		this.movieId = movieId;
+		this.moviewName = moviewName;
+		this.duration = duration;
+		this.cinemaNum = cinemaNum;
+	}
 
+	public short getMovieId() {
+		return movieId;
+	}
+
+	public String getMoviewName() {
+		return moviewName;
+	}
+
+	public float getDuration() {
+		return duration;
+	}
+
+	public byte getCinemaNum() {
+		return cinemaNum;
+	}
+	
 }
