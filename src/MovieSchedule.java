81c37abf61f904fe2db37cce1e6213854bf15e60
Ayannrwz <enriquezadrian.aws@gmail.com>
2023-10-02 14:42:27 +0800

    Modified the Movie and MovieSchedule class

--- src/MovieSchedule.java
+++ src/MovieSchedule.java
@@ -1,4 +1,40 @@
+import java.time.LocalDateTime;
 
 public class MovieSchedule {
+	private short movieSchedule;
+	private LocalDateTime showingDateTime;
+	private Movie movie;
+	private boolean isPremiereShow;
+	private SeatLayout seats;
+	
+	public MovieSchedule(short movieSchedule, LocalDateTime showingDateTime, Movie movie, boolean isPremiereShow,
+			SeatLayout seats) {
+		super();
+		this.movieSchedule = movieSchedule;
+		this.showingDateTime = showingDateTime;
+		this.movie = movie;
+		this.isPremiereShow = isPremiereShow;
+		this.seats = seats;
+	}
 
+	public short getMovieSchedule() {
+		return movieSchedule;
+	}
+
+	public LocalDateTime getShowingDateTime() {
+		return showingDateTime;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public boolean isPremiereShow() {
+		return isPremiereShow;
+	}
+
+	public SeatLayout getSeats() {
+		return seats;
+	}
+	
 }
